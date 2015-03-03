b3fb94bb23884ef30adab7e871816d65a8ccf392
Dave Voyles <davevoyles@outlook.com>
2015-03-03 08:37:32 -0500

    - Removed inline JS from HTML and moved it all to main.js - Cleaned up some HTML - Added a footer - Added a description of the project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+(function (undefined) {
+
+	// Grab inputs and button for speech-to-text
+	var form    = document.querySelector('#player-form'   ),
+		input   = document.querySelector('#player-input'  ),
+		element = document.querySelector('#player-element'),
+	    xPokemon = document.querySelector('#x-pokemon'	  );
+
+	// Take text from input & set it as the text that the speaker will say.
+	// Set the name of the pokemon, which angular will then grab from the pokemon DB 
+	input.addEventListener('input', function (e) {
+		element.setAttribute('text', input.value);
+		xPokemon.name = input.value
+	});
+
+	// Say the text when button is pressed
+	form.addEventListener('submit', function (e) {
+		e.preventDefault();
+		element.speak();
+	});
+
+})();
 
