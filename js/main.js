e87782c22902a2186c35aa7cdf34ad81fc61d82c
Dave Voyles <davevoyles@outlook.com>
2015-03-03 09:28:33 -0500

    Adding the ability to change accents

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 (function (undefined) {
 
 	// Grab inputs and button for speech-to-text
-	var form    = document.querySelector('#player-form'   ),
-		input   = document.querySelector('#player-input'  ),
-		element = document.querySelector('#player-element'),
-	    xPokemon = document.querySelector('#x-pokemon'	  );
+	var form = document.querySelector('#player-form'),
+		input = document.querySelector('#player-input'),
+		playerElement = document.querySelector('#player-element'),
+	    xPokemon = document.querySelector('#x-pokemon'),
+		btn_changeAccent = document.querySelector('#btn-change-accent');
 
 	// Take text from input & set it as the text that the speaker will say.
 	// Set the name of the pokemon, which angular will then grab from the pokemon DB 
 	input.addEventListener('input', function (e) {
-		element.setAttribute('text', input.value);
+		playerElement.setAttribute('text', input.value);
 		xPokemon.name = input.value
 	});
 
 	// Say the text when button is pressed
 	form.addEventListener('submit', function (e) {
 		e.preventDefault();
-		element.speak();
+		playerElement.speak();
 	});
+
+
+	changeAccent = function () {
+		var accentAttr = playerElement.getAttribute("accent");
+		console.log(accentAttr);
+		var Newaccent = playerElement.setAttribute("accent", "en-GB");
+		var player = document.querySelector('#player-element');
+		playerAccent = player.getAttribute("accent");
+		console.log(playerAccent);
+	};
+
+	// Event listener for changing accents
+	btn_changeAccent.addEventListener('submit', changeAccent() );
 
 })();
 
