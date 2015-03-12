1ac1c4d73d29b8f1b283ca7b76369762ebbb1010
Dave Voyles <davevoyles@outlook.com>
2015-03-11 20:41:09 -0400

    Cleaned up more code. Added more explanations. Bug: Angular is no longer listening for changes to the pokemon name

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+/* @Author: Dave Voyles, Microsoft Corp. 2015 
+ *
+ * Notice how we are grabbing Web Components here. We are taking two (2) different approaches:
+ * 1) Grabbing the id of the #player-element
+ *			<voice-player id="player-element" accent="es-ES" text=""></voice-player>
+ * This element also has an attribute called "accent". When "accent" is changed, the JavaScript within the voice-player web component
+ * listens for this, and fires off a function to change the accent.
+ * 
+ * 2) Grabbing the name of the web component
+ *		     <x-radial-buttons></x-radial-buttons>
+ * The name is defined in when first creating the polymer-element. In this case, in x-radial-buttons.html
+ * As we start with: 
+ *			  <polymer-element name="x-radial-buttons">
+ *
+ * The first way is fine if you are just grabbing a publicly exposed HTML5 tag (attribute) such as "accent".
+ * However, if you want to be able to accesss the public methods within the polymer-element, you need to use:
+ *		     document.querySelector('name-of-selector-WITHOUT-using-#-");
+ * 
+ * Now we can call on that method by using:
+ *			 var radialButtons = document.querySelector("x-radial-buttons"  );
+ *			 radialButtons.getCurrentAccent();
+ * 
+ */
+
 (function (pokemonApp) {
 
 	// Grab inputs and button for speech-to-text
@@ -27,8 +51,10 @@
 
 	// @Caller: X-radial-buttons web component
 	// Setting the accent based on the radial button the user selected
-	pokemonApp.changeAccent = function (accent) {
-		var Newaccent = playerElement.setAttribute("accent", accent);
+    pokemonApp.changeAccent = function (accent) {
+    	var currentAccent = radialButtons.getCurrentAccent();
+    	console.log("current accent is: " + currentAccent + ". " + "Changing accent to: " + accent);
+		var Newaccent    = playerElement.setAttribute("accent", accent);
 	}
 
 
