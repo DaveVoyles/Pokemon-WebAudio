79a6bbfa1aa69ccff46b65e838d42952dc8075dd
Dave Voyles <davevoyles@outlook.com>
2015-03-12 09:22:03 -0400

    Working 100%. Event Listeners are wired up correctly. Need to clean up UI now

--- js/main.js
+++ js/main.js
@@ -24,30 +24,23 @@
 
 (function (pokemonApp) {
 
-	// Grab inputs and button for speech-to-text
     var form                 = document.querySelector('#player-form'	  ),  // Contrainer for web audio
         input                = document.querySelector('#player-input'     ),  // Enter name of pokemon here
         playerElement        = document.querySelector('#player-element'	  ),  // Web component that speaks the name of the Pokemon
         xPokemon             = document.querySelector('#x-pokemon'		  ),  // Web component for grabbing the pokemon from the DB
-        radialButtons		 = document.querySelector("x-radial-buttons"),  // Notice, not grabbing the # [id]
-	    btnChangeAccent		 = document.querySelector('#btn-change-accent'),
+        radialButtons		 = document.querySelector("x-radial-buttons"  ),  // Notice, not grabbing the # [id]
        	playerAccent         = playerElement.getAttribute("accent"		  );  // Grabbing Polymer attribute
 
+	/* -- Event Listeners ------------------------------------------------ */
+    form.addEventListener('submit', function (e) { window.pokemonApp.sayTheName(e) });
+
+	input.addEventListener('input',  function (e) {
+		playerElement.setAttribute('text', input.value);
+		xPokemon.name = input.value;
+	});
 
 
-    input.addEventListener('input', function (e) {
-    	playerElement.setAttribute('text', input.value);
-    	xPokemon.name = input.value;
-    });
-
-
-
-
-    pokemonApp.changeAccent = function (accent) {
-      	var Newaccent = playerElement.setAttribute("accent", accent);
-    };
-
-
+	/* -- Functions ----------------------------------------------------- */
 	/* Sets the name of the pokemon, based on the text the user enters in the input tag
 	*  after each character, angular pokemon then checks the pokemon DB to see if such a name exists.
 	*  Also, take text from input & set it as the text that the speaker will say. */
@@ -64,21 +57,13 @@
     };
 
 
-	// @Caller: X-radial-buttons web component
-	// Setting the accent based on the radial button the user selected
-    //pokemonApp.changeAccent = function (accent) {
-    //	var radialButtons = radialButtons = document.querySelector("x-radial-buttons");
-    //	var currentAccent = radialButtons.getCurrentAccent();
-    //	console.log("current accent is: " + currentAccent + ". " + "Changing accent to: " + accent);
-	//	var Newaccent    = playerElement.setAttribute("accent", accent);
-	//}
-
-
-	/* -- Event Listeners ------------------------------------------------ */
-	//input.			addEventListener('input',				  window.pokemonApp.setName()	   );
-	form.addEventListener('submit', function (e) { window.pokemonApp.sayTheName(e) });
-	btnChangeAccent.addEventListener('submit', window.pokemonApp.changeAccent());
-
+	 //@Caller: X-radial-buttons web component
+	 //Setting the accent based on the radial button the user selected
+    pokemonApp.changeAccent = function (accent) {
+    	var currentAccent = radialButtons.getCurrentAccent();
+    	console.log("current accent is: " + currentAccent + ". " + "Changing accent to: " + accent);
+		var Newaccent     = playerElement.setAttribute("accent", accent);
+	}
 
 
 // Creates a namespace for this 'class'.
